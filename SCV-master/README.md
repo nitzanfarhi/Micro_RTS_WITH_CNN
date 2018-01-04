b9173b08e3da79843acdfc0d3ef914e6ae548a10
nitzan farhi <nitzan2611@gmail.com>
2018-01-04 09:46:20 +0200

    First Commit

---
+++ SCV-master/README.md
@@ -0,0 +1,17 @@
+# SCV
+
+MicroRTS Bot whitch uses voting and supervised Learning techniques.    
+Team members:    
+Cleyton Rodrigo da Silva - Universidade Federal de Viçosa - cleyton.rodrigo.silva@gmail.com    
+Rubens de Oliveira Moraes Filho - Universidade Federal de Viçosa - rubensolv@gmail.com    
+Levi Lelis - Universidade Federal de Viçosa - levilelis@gmail.com    
+
+For more information consult the [Wiki](https://github.com/rubensolv/PVAI-bot/wiki) .
+
+Bot MicroRTS que utiliza técnicas de votação e aprendizagem supervisionada
+Time:    
+Cleyton Rodrigo da Silva - Universidade Federal de Viçosa - cleyton.rodrigo.silva@gmail.com    
+Rubens de Oliveira Moraes Filho - Universidade Federal de Viçosa - rubensolv@gmail.com    
+Levi Lelis - Universidade Federal de Viçosa - levilelis@gmail.com    
+
+Para mais informações consulte a [Wiki](https://github.com/rubensolv/PVAI-bot/wiki).
---
+++ microrts-master/src/org/ea/javacnn/JavaCNN.java
@@ -0,0 +1,81 @@
+package org.ea.javacnn;
+
+import org.ea.javacnn.data.BackPropResult;
+import org.ea.javacnn.data.DataBlock;
+import org.ea.javacnn.layers.Layer;
+import org.ea.javacnn.losslayers.LossLayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A network class holding the layers and some helper functions
+ * for training and validation.
+ *
+ * @author Daniel Persson (mailto.woden@gmail.com)
+ */
+public class JavaCNN {
+    private List<Layer> layers;
+
+    public JavaCNN(List<Layer> layers) {
+        this.layers = layers;
+    }
+
+    /*
+     forward prop the network.
+     The trainer class passes is_training = true, but when this function is
+     called from outside (not from the trainer), it defaults to prediction mode
+    */
+    public DataBlock forward(DataBlock db, boolean training) {
+      DataBlock act = this.layers.get(0).forward(db, training);
+      for(int i=1;i<this.layers.size();i++) {
+        act = this.layers.get(i).forward(act, training);
+      }
+      return act;
+    }
+
+    public double getCostLoss(DataBlock db, int y) {
+      this.forward(db, false);
+      int N = this.layers.size();
+      double loss = ((LossLayer)this.layers.get(N-1)).backward(y);
+      return loss;
+    }
+
+    // backprop: compute gradients wrt all parameters
+    public double backward(int y) {
+      int N = this.layers.size();
+      double loss = ((LossLayer)this.layers.get(N-1)).backward(y);
+      for(int i=N-2;i>=0;i--) { // first layer assumed input
+        this.layers.get(i).backward();
+      }
+      return loss;
+    }
+
+    // accumulate parameters and gradients for the entire network
+    public List<BackPropResult> getBackPropagationResult() {
+      List<BackPropResult> result = new ArrayList<BackPropResult>();
+      for(Layer l : this.layers) {
+        List<BackPropResult> subResult = l.getBackPropagationResult();
+        result.addAll(subResult);
+      }
+      return result;
+    }
+
+    /*
+    this is a convenience function for returning the argmax
+    prediction, assuming the last layer of the net is a softmax
+    */
+    public int getPrediction() {
+      LossLayer S = (LossLayer)this.layers.get(this.layers.size()-1);
+      double[] p = S.getOutAct().getWeights();
+      double maxv = p[0];
+      int maxi = 0;
+      for(int i=1; i<p.length; i++) {
+        if(p[i] > maxv) {
+          maxv = p[i];
+          maxi = i;
+        }
+      }
+      return maxi;
+    }
+}
